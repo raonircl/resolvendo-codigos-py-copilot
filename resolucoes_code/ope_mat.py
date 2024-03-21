b81861dc539d7b43c3ce52b258427c189afdb13a
raonircl <raoni.afi@gmail.com>
2024-03-21 09:04:15 -0300

    feat: operações

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,11 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 =  int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+
+soma = num1 + num2
+mult = num1 * num2
+sub = num1 - num2
+div = num1 / num2
+
+print(f"Resultados de operações simples: Soma: {soma}, Mult: {mult}, Sub: {sub}, Div: {div}")
