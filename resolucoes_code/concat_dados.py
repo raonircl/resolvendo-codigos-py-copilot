74169f242af8ed8d67841aa208b1aefce24a104c
raonircl <raoni.afi@gmail.com>
2024-03-21 08:54:53 -0300

    feat: concatenação e repetição de texto

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+name = input("Digite seu nome: ")
+age = input("Digite sua idade: ")
+
+mensagem = f"Seu nome é: {name}, Sua idade: {age}"
+
+print(mensagem)
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,10 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Vamos solicitar uma string e um número inteiro como entrada.
+
+palavra = input("Digite uma palavra: ")
+numero = int(input("Digite um número: "))
+
+mensagem = f"Palavra: {palavra}, número de vezes que irá repetir {numero}"
+
+print(mensagem)
+print((palavra + " ") * numero)
+
